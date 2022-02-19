1cb7681622392e5a1266b18fc3dc2f2c9c0690bd
Grayson Miller <graysonmiller@Graysons-MacBook-Pro.local>
2022-02-18 20:20:48 -0500

    .

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//necessary for using handlebars.
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+// express.static() is a built in express.js middleware that takes all the contents of a folder and serves them as static assets
 
 // turn on routes
 app.use(routes);
